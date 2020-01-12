065936a963d8ac1f55d76c155ea738793740d567
Sheng-Jie106 <55180712+Sheng-Jie106@users.noreply.github.com>
2020-01-12 23:29:17 +0800

    1

---
+++ assert/js/slow.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+$('a[href*="#"]:not([href="#"])').click(function() {
+var offset = -74;
+if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+var target = $(this.hash);
+target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+if (target.length) {
+$('html, body').animate({
+scrollTop: target.offset().top + offset
+}, 1000);
+return false;
+}
+}
+});
+});
+window.onload=function(){
+var body=document.getElementById("body");
+body.style.opacity=1;
+}
